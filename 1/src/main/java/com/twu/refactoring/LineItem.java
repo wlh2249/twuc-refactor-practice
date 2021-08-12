68cf1b5f7fd2fd40d4038bc1a54cd7219b8955db
wlh2249 <wlh2249@gmail.com>
2021-08-13 02:46:59 +0800

    [huweilin] refactor: rename LineItem in 1

--- 1/src/main/java/com/twu/refactoring/LineItem.java
+++ 1/src/main/java/com/twu/refactoring/LineItem.java
@@ -1,30 +1,30 @@
 package com.twu.refactoring;
 
 public class LineItem {
-	private String desc;
-	private double p;
-	private int qty;
+	private final String description;
+	private final double price;
+	private final int quantity;
 
-	public LineItem(String desc, double p, int qty) {
+	public LineItem(String desc, double p, int quantity) {
 		super();
-		this.desc = desc;
-		this.p = p;
-		this.qty = qty;
+		this.description = desc;
+		this.price = p;
+		this.quantity = quantity;
 	}
 
 	public String getDescription() {
-		return desc;
+		return description;
 	}
 
 	public double getPrice() {
-		return p;
+		return price;
 	}
 
 	public int getQuantity() {
-		return qty;
+		return quantity;
 	}
 
     double totalAmount() {
-        return p * qty;
+        return price * quantity;
     }
 }
